bb9cdd1055c6a69917a4736ee63833d197f1df3f
vladovoev <vladovoev@yahoo.co.uk>
2018-01-17 14:13:11 +0200

    initial commit and some coding tools

---
+++ GigProject/GigProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GigProject.MvcApplication" Language="C#" %>
---
+++ GigProject/GigProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigProject.Startup))]
+namespace GigProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
